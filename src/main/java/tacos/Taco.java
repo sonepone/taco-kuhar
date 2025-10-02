0e9b9e121a5d4dd95a91b6fe28fc7c4d30f597e3
Nebojsa Stefanovic <cordunas@gmail.com>
2025-10-02 12:21:07 +0200

    Dodati prvi file-ovi domena.

---
+++ src/main/java/tacos/Taco.java
@@ -0,0 +1,24 @@
+package tacos;
+
+import java.util.Date;
+import java.util.List;
+//import javax.validation.constraints.NotNull;
+//import javax.validation.constraints.Size;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class Taco {
+	
+	Long id;
+	Date createdAt;
+	
+	@NotNull
+	@Size(min = 5, message = "Name must be at least 5 characters long")
+	private String name;
+	@NotNull
+	@Size(min = 1, message = "You must choose at least 1 ingredient")
+	private List<Ingredient> ingredients;
+}
